071c30a251dd18696da2dd0f534a9e415833bb8d
Rschz <rschz.dev@gmail.com>
2022-01-06 22:06:23 -0400

    login-guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Personas-App';
+
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAsOp2D6yv0VTpOfhOeTCZqkhdKEQZSPpY',
+      authDomain: 'listadopersonas-cefbc.firebaseapp.com',
+    };
+
+    const app = initializeApp(firebaseConfig);
+  }
+
+  isLogged() {
+    return this.loginService.isLogged();
+  }
+
+  logout() {
+    this.loginService.logout();
+  }
 }
